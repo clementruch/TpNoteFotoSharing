951f3a636a9df56d1c1fcd5e3d47408c6388f970
clementruch <144322316+clementruch@users.noreply.github.com>
2024-11-29 14:06:04 +0100

    Initial commit

---
+++ src/main/java/local/epul4a/tpnotefotosharing/TpNoteFotoSharingApplication.java
@@ -0,0 +1,13 @@
+package local.epul4a.tpnotefotosharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpNoteFotoSharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpNoteFotoSharingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/local/epul4a/tpnotefotosharing/TpNoteFotoSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package local.epul4a.tpnotefotosharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpNoteFotoSharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
